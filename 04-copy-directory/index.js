237b8df79bdf6f9fa310f8bbdd314696529d4609
Viktoryia Soroka <v.soroka@live.com>
2022-05-23 23:53:05 +0200

    feat: add function 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,27 @@
+const fs = require('fs');
+const path = require('path');
 
+function copyDir() {
+fs.mkdir(path.join(__dirname, 'files-copy'), {recursive: true}, (err) => {
+    if (err) throw err;
+});
+fs.readdir(path.join(__dirname, 'files-copy'), {withFileTypes: true}, (err, files) => {
+    if (err) throw err;
+    for (let file of files) {
+fs.unlink(path.join(path.join((__dirname, 'files-copy'), file.name), (err) => {
+    if (err) throw err;
+})
+)}
+fs.readdir(path.join(__dirname, 'files'), {withFileTypes: true}, (err, files) => {
+    if (err) throw err;
+    for (let file of files) {
+        fs.copyFile(path.join(path.join(__dirname, 'files'), file.name), path.join(path.join(__dirname, 'files-copy'), file.name), (err) => {
+            if (err) throw err;
+        })
+    }
+})
+}
+);
+};
+
+copyDir();
