da366a3d50f46f0f9206128589ae642c8c30087c
Viktoryia Soroka <v.soroka@live.com>
2022-05-25 11:31:39 +0200

    fix: change function 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,27 +1,29 @@
 const fs = require('fs');
 const path = require('path');
 
-function copyDir() {
-fs.mkdir(path.join(__dirname, 'files-copy'), {recursive: true}, (err) => {
-    if (err) throw err;
-});
-fs.readdir(path.join(__dirname, 'files-copy'), {withFileTypes: true}, (err, files) => {
-    if (err) throw err;
-    for (let file of files) {
-fs.unlink(path.join(path.join((__dirname, 'files-copy'), file.name), (err) => {
-    if (err) throw err;
-})
-)}
-fs.readdir(path.join(__dirname, 'files'), {withFileTypes: true}, (err, files) => {
-    if (err) throw err;
-    for (let file of files) {
-        fs.copyFile(path.join(path.join(__dirname, 'files'), file.name), path.join(path.join(__dirname, 'files-copy'), file.name), (err) => {
+const origDirect = path.join(__dirname, 'files');
+const copyDirect = path.join(__dirname, 'files-copy');
+
+async function copyDir() {
+    await fs.promises.rm(copyDirect, {recursive: true, force: true});
+    fs.mkdir(copyDirect, {recursive: true}, (err) => {
+      if (err) throw err;
+    });
+    fs.readdir(origDirect, {withFileTypes: true}, (err, files) => {
+      if (err) throw err;
+      files.forEach(file => {
+        if(file.isFile()) {
+          fs.copyFile(path.join(origDirect, file.name), path.join(copyDirect, file.name), (err) => {
             if (err) throw err;
-        })
-    }
-})
-}
-);
-};
+          });
+        } else if(file.isDirectory()) {
+          fs.mkdir(path.join(copyDirect, file.name), {recursive: true}, (err) => {
+            if (err) throw err;
+          });
+          copyFile(path.join(origDirect, file.name), path.join(copyDirect, file.name));
+        }
+      });
+    });
+  }
 
 copyDir();
